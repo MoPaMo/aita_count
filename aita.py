7f9f5b46d141af0c3bc2201b4b430d39243ac8d4
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-01 22:39:53 +0200

    chore: Improve determination of verdict in process_post function

--- aita.py
+++ aita.py
@@ -30,7 +30,7 @@ def get_verdict_from_comment(comment_body):
     elif 'nah' in comment_body:  # 'NAH' stands for "No Assholes Here"
         return 'NAH'
     else:
-        return 'N/A'  # In case the comment does not clearly state any known verdict
+        return "N/A"  # In case the comment does not clearly state any known verdict
 
 # Function to convert Unix timestamp to a readable date and time format
 def convert_timestamp(timestamp):
@@ -41,18 +41,23 @@ def convert_timestamp(timestamp):
 def process_post(post):
     post.comments.replace_more(limit=0)  # Fetch all comments
     if len(post.comments) > 0:
-        top_comment = post.comments[1]  # Get the topmost comment
-        verdict = get_verdict_from_comment(top_comment.body)
-        if verdict is not None:
-            date_time = convert_timestamp(post.created).split()
-            return {
-                'post_id': post.id,
-                'score': post.score,
-                'top_comment_id': top_comment.id,
-                'verdict': verdict,
-                'date': date_time[0],  # Date part
-                'time': date_time[1]  # Time part
-            }
+        verdict = "N/A"
+        top_comment_num = 1
+        while verdict is "N/A":  # Keep trying to determine the verdict until it is determined
+            top_comment = post.comments[top_comment_num]  # Get the topmost comment
+            verdict = get_verdict_from_comment(top_comment.body)
+            top_comment_num = top_comment_num + 1
+            if len(post.comments) <= top_comment_num:
+                break
+        date_time = convert_timestamp(post.created).split()
+        return {
+            'post_id': post.id,
+            'score': post.score,
+            'top_comment_id': top_comment.id,
+            'verdict': verdict,
+            'date': date_time[0],  # Date part
+            'time': date_time[1]  # Time part
+        }
     return None
 
 # Fetch top posts from the subreddit for the past year
