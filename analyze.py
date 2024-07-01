3160bfc0951743a6487f17f2a02fc6c287319996
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-01 23:36:41 +0200

    Refactor process_post function to improve verdict determination and performance

--- analyze.py
+++ analyze.py
@@ -4,15 +4,16 @@ print(df.head())
 print(f"Number of rows in aita_results.csv: {len(df)}")
 
 print(df.verdict.value_counts())
-
+#relative distribution
+print(df.verdict.value_counts(normalize=True))
 # only store hour for time
 df['hour'] = df['time'].str.split(':').str[0]
 
 
-print(df.groupby(df.hour).verdict.value_counts())
+print(df.groupby(df.hour).verdict.value_counts(normalize=True))
 
 # only store month for date
 df['month'] = df['date'].str.split('-').str[1]
 
 
-print(df.groupby(df.month).verdict.value_counts())
+print(df.groupby(df.month).verdict.value_counts(normalize=True))
