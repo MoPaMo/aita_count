bf9b14fd3484baf6143d36c564c5e8f4852a8aed
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-07-01 23:11:33 +0200

    analyze data

--- analyze.py
+++ analyze.py
@@ -2,3 +2,17 @@ import pandas as pd
 df = pd.read_csv('aita_results.csv')
 print(df.head())
 print(f"Number of rows in aita_results.csv: {len(df)}")
+
+print(df.verdict.value_counts())
+
+# only store hour for time
+df['hour'] = df['time'].str.split(':').str[0]
+
+
+print(df.groupby(df.hour).verdict.value_counts())
+
+# only store month for date
+df['month'] = df['date'].str.split('-').str[1]
+
+
+print(df.groupby(df.month).verdict.value_counts())
